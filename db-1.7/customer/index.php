f37d22f74092cba615d67e2e11eebb66697a519e
akirafz1 <akira.fz1@gmail.com>
2017-04-02 11:03:07 +0900

    Add files via upload

---
+++ db-1.7/customer/index.php
@@ -0,0 +1,87 @@
+<?php
+/*
+ * Copyright(c) 2009 limitlink,Inc. All Rights Reserved.
+ * http://limitlink.jp/
+ * 文字コード UTF-8
+ */
+require_once('../application/loader.php');
+$view->script('postcode.js');
+$view->heading('顧客情報');
+$pagination = new Pagination(array('folder'=>$_GET['folder'],'jufuku'=>$_GET['jufuku']));
+$liquid = new Liquid;
+?>
+<div class="contentcontrol">
+	<h1>顧客一覧・検索<?=$view->caption($hash['folder'], array('all'=>'すべて表示'))?></h1>
+	<table class="customertype" cellspacing="0"><tr>
+<!--		<td><a class="current" href="index.php">・</a></td>
+		<td><a href="company.php">法人</a></td>  -->
+	</tr></table>
+	<div class="clearer"></div>
+</div>
+<ul class="operate">
+<?php
+if ($view->permitted($hash['category'], 'add')) {
+	echo '<li><a href="add.php?b_url=1&'.$view->parameter(array('folder'=>$_GET['folder'])).'">顧客追加</a></li>';
+}
+if (count($hash['list']) <= 0) {
+	$attribute = ' onclick="alert(\'出力するデータがありません。\');return false;"';
+}
+//	var_dump($hash['count']);
+?>
+
+<!--
+<?php
+if ($view->authorize('administrator', 'manager')) {
+	echo '<li><a href="config.php?folder='.intval($_GET['folder']).'">顧客設定</a></li>';
+}
+?>
+-->
+
+<li><a href="csv.php<?=$view->parameter(array('sort'=>$_GET['sort'], 'desc'=>$_GET['desc'], 'search'=>$_GET['search'], 'jufuku'=>$_GET['jufuku'], 'folder'=>$_GET['folder'], 'type'=>0))?>"<?=$attribute?>>宛名ダウンロード</a></li>
+
+<?php
+//	echo intval($_GET['folder']).'folder';
+?>
+
+<li><a href="index.php<?=$view->parameter(array('folder'=>$_GET['folder'],'jufuku'=>1))?>">重複顧客</a></li>
+
+<li style="color:#FF0000;">(顧客追加前に過去の顧客を検索してね！)</li>
+
+</ul>
+<?=$view->searchformime(array('folder'=>$_GET['folder']))?>
+
+<table class="content" cellspacing="0">
+<tr><td class="contentfolder">
+	<?=$view->category($hash['folder'], 'customer')?>
+</td><td>
+	<table class="list" cellspacing="0">
+		<tr>
+		<th><?=$pagination->sortby('id', '顧客ID')?></th>
+		<th><?=$pagination->sortby('customer_name', '名前')?></th>
+		<th><?=$pagination->sortby('customer_company', '会社名')?></th>
+		<th><?=$pagination->sortby('customer_phone', '電話番号')?></th>
+		<th><?=$pagination->sortby('customer_mobile', '携帯番号')?></th>
+		<th><?=$pagination->sortby('customer_workphone', '会社TEL')?></th>
+		</tr>
+<?php
+if (is_array($hash['list']) && count($hash['list']) > 0) {
+	foreach ($hash['list'] as $row) {
+?>
+		<tr>
+		<td><?=$row['id']?></a>&nbsp;</td>
+		<td><a href="../history/customer.php?parent=<?=$row['id']?>&b_url=1"><?=$row['customer_name']?></a>&nbsp;</td>
+		<td><?=$row['customer_company']?></a>&nbsp;</td>
+		<td><?=$row['customer_phone']?></a>&nbsp;</td>
+		<td><?=$row['customer_mobile']?></a>&nbsp;</td>
+		<td><?=$row['customer_workphone']?></a>&nbsp;</td>
+		</tr>
+<?php
+	}
+}
+?>
+	</table>
+	<?=$view->pagination($pagination, $hash['count'])?>
+</td></tr></table>
+<?php
+$view->footing();
+?>
---
+++ db-1.7/history/index.php
@@ -0,0 +1,94 @@
+<?php
+/*
+ * Copyright(c) 2009 limitlink,Inc. All Rights Reserved.
+ * http://limitlink.jp/
+ * 文字コード UTF-8
+ */
+require_once('../application/loader.php');
+$view->script('postcode.js');
+$view->heading('顧客情報');
+$pagination = new Pagination(array('folder'=>$_GET['folder']));
+$liquid = new Liquid;
+$_SESSION['backurl'] = '';
+$_SESSION['backurl2'] = '';
+?>
+<div class="contentcontrol">
+	<h1>車両一覧・検索<?=$view->caption($hash['folder'], array('all'=>'すべて表示'))?></h1>
+	<table class="customertype" cellspacing="0"><tr>
+<!--		<td><a class="current" href="index.php">・</a></td>
+		<td><a href="company.php">法人</a></td>  -->
+	</tr></table>
+	<div class="clearer"></div>
+</div>
+<ul class="operate">
+<?php
+if ($view->permitted($hash['category'], 'add')) {
+//	echo '<li><a href="add.php'.$view->parameter(array('folder'=>$_GET['folder'])).'">顧客追加</a></li>';
+}
+if (count($hash['list']) <= 0) {
+	$attribute = ' onclick="alert(\'出力するデータがありません。\');return false;"';
+}
+?>
+
+<!--
+<li><a href="csv.php<?=$view->parameter(array('sort'=>$_GET['sort'], 'desc'=>$_GET['desc'], 'jufuku'=>$_GET['jufuku'], 'folder'=>$_GET['folder'], 'type'=>0, 'cust_id'=>$cust_idt))?>"<?=$attribute?>>顧客一覧ダウンロード</a></li>
+-->
+
+<li><a href="csv.php<?=$view->parameter(array('sort'=>$_GET['sort'], 'desc'=>$_GET['desc'], 'search'=>$_GET['search'], 'jufuku'=>$_GET['jufuku'], 'folder'=>$_GET['folder'], 'type'=>0))?>"<?=$attribute?>>宛名ダウンロード</a></li>
+
+<?php
+// var_dump($hash['list']);
+//		echo $_GET['search'];
+//	echo intval($_GET['search']);
+
+if ($view->authorize('administrator', 'manager')) {
+//	echo '<li><a href="config.php?folder='.intval($_GET['folder']).'">顧客設定</a></li>';
+}
+?>
+
+</ul>
+<?=$view->searchform(array('folder'=>$_GET['folder']))?>
+<table class="content" cellspacing="0"><tr><td class="contentfolder">
+	<?=$view->category($hash['folder'], 'customer')?>
+	</td><td>
+	<table class="list" cellspacing="0">
+		<tr>
+		<th><?=$pagination->sortby('customer_id', '顧客ID')?></th>
+		<th><?=$pagination->sortby('customer_name', '名前')?></th>
+<!--		<th><?=$pagination->sortby('id', '車両ID')?></th> -->
+		<th><?=$pagination->sortby('history_adddate', '登録日')?></th>
+		<th><?=$pagination->sortby('history_machine', '車名')?></th>
+		<th><?=$pagination->sortby('history_color', '車体色')?></th>
+		<th><?=$pagination->sortby('history_regnum', '登録番号')?></th>
+		<th><?=$pagination->sortby('history_carnum', '車体番号')?></th>
+		<th><?=$pagination->sortby('history_gokinum', '号機番号')?></th>
+		<th><?=$pagination->sortby('history_shaken', '車検日')?></th>
+		</tr>
+<?php
+if (is_array($hash['list']) && count($hash['list']) > 0) {
+	foreach ($hash['list'] as $row) {
+?>
+		<tr>
+		<td><?=$row['customer_id']?>&nbsp;</td>
+		<td><a href="../history/customer.php?parent=<?=$row['customer_id']?>&b_url=1"><?=$row['customer_name']?></a>&nbsp;</td>
+<!--		<td><?=$row['id']?>&nbsp;</td> -->
+		<td><?=$row['history_adddate']?>&nbsp;</td>
+		<td><a href="../history/view.php?id=<?=$row['id']?>&b_url=2"><?=$row['history_machine']?></a>&nbsp;</td>
+		<td><?=$row['history_color']?>&nbsp;</td>
+		<td><?=$row['history_regnum']?>&nbsp;</td>
+		<td><?=$row['history_carnum']?>&nbsp;</td>
+		<td><?=$row['history_gokinum']?>&nbsp;</td>
+		<td><?=$row['history_shaken']?>&nbsp;</td>
+		</tr>
+<?php
+	}
+}
+?>
+	</table>
+	<?=$view->pagination($pagination, $hash['count'])?>
+	</td></tr>
+</table>
+
+<?php
+$view->footing();
+?>
